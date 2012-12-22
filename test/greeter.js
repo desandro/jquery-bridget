671e82be6695a01d8d672b1b9c0b8733dd8e8a16
David DeSandro <desandrocodes@gmail.com>
2012-12-22 11:56:10 -0500

    add greeter.js for example code

---
+++ test/greeter.js
@@ -0,0 +1,39 @@
+( function( window, $ ) {
+
+'use strict';
+
+// Create a plugin constructor class
+function NiceGreeter( element, options ) {
+  this.element = $(element);
+  // conveinence method to extend options over defaults
+  this._setIntitialOptions( options );
+  // widget properties
+  this.helloCount = 0;
+  this._init()
+}
+
+// bridget converts the constructor to a jQuery plugin
+$.bridget( 'greeter', NiceGreeter );
+
+// set defaults for plugin options
+NiceGreeter.defaults = {
+  greeting: 'hello',
+  recipient: 'world'
+};
+
+NiceGreeter.prototype._init = function() {
+  this.sayHi()
+};
+
+// enable plugin methods
+// $elem.myPluginWidget( 'sayHi', 'Bridget, darling' );
+NiceGreeter.prototype.sayHi = function( recipient ) {
+  recipient = recipient || this.options.recipient;
+  this.element.text( this.options.greeting + recipient );
+  this.helloCount++;
+  console.log( 'Said ' + this.options.greeting + ' ' + this.helloCount + ' times' );
+};
+
+
+})( window, jQuery );
+
